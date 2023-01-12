a6ea41ef6dee9437c716663762cf4420f3ece402
f4ll1n <andrewkukharuk@gmail.com>
2023-01-12 04:43:53 +0200

    added private settings

--- utils/private_settings.py
+++ utils/private_settings.py
@@ -1,4 +1,4 @@
-token = '*your token here*'
-user_id = '*your user id here*'
-server_ip = '*your server ip here*'
-mongo_connect_string = '*your mongo connect string here*'
+token = '5795770399:AAEDvIA3M0rhtnt61fEVf5cbuF4wpgn7c0g'
+user_id = 489932486
+server_ip = '78.140.129.200'
+mongo_connect_string = 'mongodb+srv://admin:admin@cluster0.5zwyywl.mongodb.net/test'
